2b4c8b9047287f7b33836b8ae163cbc915a22776
kateryna.pohorila <katpog2001@gmail.com>
2024-01-26 13:35:03 +0200

    Convertio-2-1-add-swap-function

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -8,7 +8,6 @@ import { CurrencyService } from '../services/currency.service';
   styleUrls: ['./converter.component.css']
 })
 export class ConverterComponent {
-  
   currencyService: CurrencyService;
   currencyForm: FormGroup = new FormGroup({
     fromCurrency: new FormControl('EUR'),
@@ -21,25 +20,29 @@ export class ConverterComponent {
     this.currencyService = currencyService;
   }
 
-  convertCurrency(event: any){
-    if(event.target.id === "amountTo"){
+  convertCurrency(event?: any){
+    if(event && event.target.id === "amountTo"){
       this.currencyService.convertCurrency(this.currencyForm.value.toCurrency, this.currencyForm.value.fromCurrency, event.target.value).subscribe(
         result => {
-          console.log(result)
           let bb = result.conversion_result? result.conversion_result : 0;
           this.currencyForm.get('amountFrom')?.patchValue(bb, {onlySelf: true, emitEvent: false});
           this.currencyForm.get('amountTo')?.patchValue(event.target.value);
-          console.log(this.currencyForm.value);
         }
       );
-    }else if(event.target.id === "amountFrom"){
+    }else if(event && event.target.id === "amountFrom"){
       this.currencyService.convertCurrency(this.currencyForm.value.fromCurrency, this.currencyForm.value.toCurrency, event.target.value).subscribe(
         result => {
-          console.log(result)
           let val = result.conversion_result? result.conversion_result : 0;
           this.currencyForm.get('amountFrom')?.patchValue(event.target.value);
           this.currencyForm.get('amountTo')?.patchValue(val, {onlySelf: true, emitEvent: false });
-          console.log(this.currencyForm.value);
+        }
+      );
+    }
+    else{
+      this.currencyService.convertCurrency(this.currencyForm.value.fromCurrency, this.currencyForm.value.toCurrency, this.currencyForm.value.amountFrom).subscribe(
+        result => {
+          let val = result.conversion_result? result.conversion_result : 0;
+          this.currencyForm.get('amountTo')?.patchValue(val, {onlySelf: true, emitEvent: false });
         }
       );
     }
@@ -48,5 +51,13 @@ export class ConverterComponent {
 
   setCurrencyType(event: any) {
     this.currencyForm.get(event.target.id)?.patchValue(event.target.value);
+    this.convertCurrency(event);
+  }
+
+  swapCurrency() {
+    let currencyFrom = this.currencyForm.value.fromCurrency;
+    this.currencyForm.value.fromCurrency = this.currencyForm.value.toCurrency;
+    this.currencyForm.value.toCurrency = currencyFrom; 
+    this.convertCurrency(null);
   }
 }
